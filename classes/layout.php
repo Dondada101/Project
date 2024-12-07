030f906fa89bb04a4fcb3270509d60ae866a6b8f
Dondada101 <raodileudonne@gmail.com>
2024-12-07 15:01:41 +0300

    Modified classes\layout.php

---
+++ classes/layout.php
@@ -0,0 +1,27 @@
+<?php
+
+class layout{
+    public function header(){
+      ?>
+
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="./styles/style1.css">
+      </head>
+      <body>
+        <div class="web-skeleton">
+      <?php
+    }
+    public function footer(){
+      ?>
+      </div>
+      </body>
+     </html>
+     <?php
+    }
+
+}
