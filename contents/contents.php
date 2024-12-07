cc24c0a6ff1384d13277e0d7c598efa51923527f
Dondada101 <raodileudonne@gmail.com>
2024-12-07 15:02:57 +0300

    Modified contents\contents.php

---
+++ contents/contents.php
@@ -0,0 +1,34 @@
+<?php
+
+class contents{
+  public function navBar(){
+    ?>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Home</a></li>
+      <li><a href="">Home</a></li>
+      <li><a href="">Home</a></li>
+    </ul>
+    <?php 
+  }
+  public function homePage(){
+    ?>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Home</a></li>
+      <li><a href="">Home</a></li>
+      <li><a href="">Home</a></li>
+    </ul>
+    <?php 
+  }
+  public function about(){
+    ?>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Home</a></li>
+      <li><a href="">Home</a></li>
+      <li><a href="">Home</a></li>
+    </ul>
+    <?php 
+  }
+}
