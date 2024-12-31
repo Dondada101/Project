f310d8c3e77f9758c47be88dbd1011a27c7d2e91
Dondada101 <raodileudonne@gmail.com>
2024-12-31 17:53:26 +0300

    SUF

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-  <link rel="stylesheet" href="styles/style1.css">
+  <link rel="stylesheet" href="styles/style2.css">
 </head>
 <body>
   <div class="container d-flex justify-content-centre align-items-center min-vh-100">
@@ -17,7 +17,31 @@
        
       </div>
       <div class="col-md-6">
-
+        <div class="row align-items-centre">
+          <div class="header-text mb-4">
+            <p class="h3 " style="color: blue;">Sign Up</p>
+          </div>
+          <form action="" method="post">
+          <div class="imput-group mb-3">
+            <input type="text" class="form-control form-control-lg bg-light fs-6 " placeholder="Username" name="username" required>
+          </div>
+          <div class="imput-group mb-3">
+            <input type="email" class="form-control form-control-lg bg-light fs-6 "  name="email" placeholder="Email Address" required>
+          </div>
+          <div class="imput-group mb-3">
+            <input type="password" class="form-control form-control-lg bg-light fs-6 " placeholder="Password" name="password" required>
+          </div>
+          <div class="imput-group mb-3">
+            <input type="password" class="form-control form-control-lg bg-light fs-6 " placeholder="Confirm Password" required>
+          </div>
+          <div class="input-group mb-3">
+            <button type="submit" class="btn btn-lg btn-primary w-100 fs-6">Login</button>
+          </div>
+          </form>
+          <div class="row">
+            <small>Have an Account? <a href="">Login</a></small>
+          </div>
+        </div>
       </div>
     </div>
   </div>
