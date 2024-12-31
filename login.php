90e91dca75b15403ebe12b59b3a3d5de77cac6b4
Dondada101 <raodileudonne@gmail.com>
2024-12-31 17:09:10 +0300

    CSS

--- login.php
+++ login.php
@@ -4,8 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+  <link rel="stylesheet" href="styles/style1.css">
 </head>
 <body>
-  
+  <div class="container d-flex justify-content-centre align-items-center min-vh-100">
+    <div class="row border rounder-5 p-3 bg-white shadow box-area">
+      <div class="col-md-6">
+        <div class="image">
+        <img src="images/apiProject.jpg" class="image-fluid" style="width:250px">
+        </div>
+       
+      </div>
+      <div class="col-md-6">
+
+      </div>
+    </div>
+  </div>
+  <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
 </body>
 </html>
