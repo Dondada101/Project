d6127f15d6dc4cdfcbdfda7d2f0609e91aaac600
Dondada101 <raodileudonne@gmail.com>
2025-01-05 18:35:58 +0300

    Mailer

--- classes/login.class.php
+++ classes/login.class.php
@@ -1,8 +1,15 @@
 <?php
+require 'vendor/autoload.php'; 
+use PHPMailer\PHPMailer\PHPMailer; 
+use PHPMailer\PHPMailer\Exception; 
+use Kinde\TwoFactorAuth\TwoFactorAuth;
 
 class Login extends Conn {
 
+    private $twoFactorAuth;
+
     protected function getuser($email,$pw){
+        $vCode=generateCode();
         $stmt = $this->connect()->prepare("SELECT upassword FROM users WHERE email=?;");
         
         if(!$stmt->execute(array($email))){
@@ -38,6 +45,8 @@ class Login extends Conn {
              exit(); 
             }
             $user=$stmt->fetchAll(PDO::FETCH_ASSOC);
+            
+            sendMail($email,$vCode);
             session_start();
             $_SESSION["userid"]=$user[0]["id"];
             $_SESSION['un']=$user[0]['uname'];
@@ -46,5 +55,38 @@ class Login extends Conn {
         
            $stmt=null;
      }
-  
+    
+     public function sendMail($email, $verification_code){
+    $mail = new PHPMailer(true);
+    $verification_code=generateCode();
+
+    try {
+        //Server settings
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'lisledon944@gmail.com';                     //SMTP username
+        $mail->Password   = 'YOUR_PASSWORD_SHOULD_GO_HERE';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        //Recipients
+        $mail->setFrom('lisledon944@gmail.com', 'CodingShodingWithNJ');
+        $mail->addAddress($email);     //Add a recipient
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = "Thanks for Registering with us. To activate your account click $verification_code";
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        // echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo json_encode(['status' => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
+    }
+}
+
+    public function generateCode(){
+        return rand(100000, 999999);
+ }
 }
--- includes/login.php
+++ includes/login.php
@@ -2,9 +2,9 @@
 if($_SERVER["REQUEST_METHOD"]==="POST"){
   $email=htmlspecialchars($_POST['email'],ENT_QUOTES,'UTF-8');
   $pw=htmlspecialchars($_POST['password'],ENT_QUOTES,'UTF-8');
-  require "../classess/conn.class.php";
-  require "../classess/login.class.php";
-  require "../classess/logctrl.class.php";
+  require "../classes/conn.class.php";
+  require "../classes/login.class.php";
+  require "../classes/logctrl.class.php";
   $su=new Logctrl($email,$pw);
   $su->login();
   header("location: ../index.php");
