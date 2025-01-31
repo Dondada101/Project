18c96caf87f0d35e6587fbb5bca8269d332f24bb
Dondada101 <raodileudonne@gmail.com>
2025-01-31 23:48:31 +0300

    ADMIN page

--- admin.php
+++ admin.php
@@ -3,12 +3,29 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Arrows&family=Jaro:opsz@6..72&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="styles/admin.css">
+  <title>Admin</title>
 </head>
 <body>
   <div class="container">
-    <div id="header"></div>
-    <div id="navbar"></div>
+    <div id="header">
+      <p><a href="">Read</a></p>
+      <p><a href="">Insert</a></p>
+      <p><a href="">Update</a></p>
+      <p><a href="">Delete</a></p>
+    </div>
+    <div id="navbar">
+      <div class="logo">
+      <p>ICheck</p>
+      </div>
+      <!-- <div class="output">
+        <p><a href="">Analytics</a></p>
+        <p><a href="">Report</a></p>
+      </div> -->
+    </div>
     <div id="content"></div>
     <div id="footer"></div>
   </div>
