e65b091e682cffb719b874d1aade3a8d7f3d3ddf
Dondada101 <raodileudonne@gmail.com>
2025-02-10 19:17:10 +0300

    Object instantiation

--- includes/adminOp.php
+++ includes/adminOp.php
@@ -5,5 +5,9 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
   $specialization=htmlspecialchars($_POST['specialization'],ENT_QUOTES,'UTF-8');
   $sspecialization=htmlspecialchars($_POST['sspecialization'],ENT_QUOTES,'UTF-8');
   $dPassword =bin2hex(random_bytes(20 / 2));
-
+  require '../classes/admin/adminOp.class.php';
+  require '../classes/admin/adminOpctrl.class.php';
+  require '../classes/conn.class.php';
+  $newDoctor=new AdminOpCtrl($dName,$dEmail,$dPassword,$specialization,$sspecialization);
+  $newDoctor->addDoctorDetails();
 }
