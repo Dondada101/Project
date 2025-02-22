c2bf38879c84a3fd23df46cf2eed056908f292bf
Dondada101 <raodileudonne@gmail.com>
2025-02-22 14:06:23 +0300

    Function to get hospital name and number of request

--- classes/reportsngraphs.class.php
+++ classes/reportsngraphs.class.php
@@ -8,4 +8,12 @@ class ReportnGraphs extends Conn{
       return $data;
     }
   }
+  protected function getHosReqCount(){
+    $sql='SELECT h.hname ,hrc.reqCount FROM hospitalrequestcount AS hrc INNER JOIN hospitals AS h ON(hrc.hid=h.hid ';
+    $stmt=$this->connect()->prepare($sql);
+    if($stmt->execute()){
+      $data=$stmt->fetchAll(PDO::FETCH_ASSOC);
+      return $data;
+    }
+  }
 }
