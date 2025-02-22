f5915861dda336eb80b983372653358b62df4c6c
Dondada101 <raodileudonne@gmail.com>
2025-02-22 14:00:01 +0300

    function to get number of doctor request count and doctor name

--- classes/reportsngraphs.class.php
+++ classes/reportsngraphs.class.php
@@ -1,4 +1,11 @@
 <?php
 class ReportnGraphs extends Conn{
-  
+  protected function getDocReqCount(){
+    $sql='SELECT d.dname ,drc.reqCount FROM doctorrequestcount AS drc INNER JOIN doctordetails AS d ON(drc.did=d.did ';
+    $stmt=$this->connect()->prepare($sql);
+    if($stmt->execute()){
+      $data=$stmt->fetchAll(PDO::FETCH_ASSOC);
+      return $data;
+    }
+  }
 }
